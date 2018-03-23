e6b45950c39353b4020ca2c7bd057af894156c0a
kevin.blakeley <kevin.blakeley@CRAIWS07.ecenet.ece.uprm.edu>
2018-03-23 02:10:21 -0400

    P4 Done

--- DataStructuresP1/src/p1MainClasses/Part1Main.java
+++ DataStructuresP1/src/p1MainClasses/Part1Main.java
@@ -2,16 +2,19 @@ package p1MainClasses;
 
 import java.io.FileNotFoundException;
 
+import javax.naming.InvalidNameException;
+
 import dataGenerator.DataReader;
 import interfaces.MySet;
 import mySetImplementations.Set1;
 import mySetImplementations.Set2;
 import solutions.P1P2Solution;
 import solutions.P3Solution;
+import solutions.P4Solution;
 
 public class Part1Main<E> {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, InvalidNameException {
 		
 		DataReader dr = new DataReader();
 		Integer[][][] data = (Integer[][][]) dr.readDataFiles();
@@ -19,6 +22,7 @@ public class Part1Main<E> {
 		P1P2Solution<Integer> finder = new P1P2Solution<Integer>("1");
 		P1P2Solution<Integer> finder2 = new P1P2Solution<Integer>("2");
 		P3Solution<Integer> finder3 = new P3Solution<Integer>("3");
+		P4Solution<Integer> finder4 = new P4Solution<Integer>("4");
 		
 		MySet<Integer>[] unions = (MySet<Integer>[]) toSetArray1(data);
 		Set1<Integer> set = (Set1<Integer>) finder.intersectSets(unions);
@@ -28,9 +32,13 @@ public class Part1Main<E> {
 		Set2<Integer> set2 = (Set2<Integer>) finder2.intersectSets(unions2);
 		System.out.println("Final set by P2: " + set2);	
 		
-		MySet<Integer>[] unions3 = (MySet<Integer>[]) toSetArray2(data);
-		Set2<Integer> set3 = (Set2<Integer>) finder3.intersectSets(unions3);
-		System.out.println("Final set by P3: " + set3);	
+//		MySet<Integer>[] unions3 = (MySet<Integer>[]) toSetArray2(data);
+//		Set2<Integer> set3 = (Set2<Integer>) finder3.intersectSets(unions3);
+//		System.out.println("Final set by P3: " + set3);	
+		
+		MySet<Integer>[] unions4 = (MySet<Integer>[]) toSetArray2(data);
+		Set2<Integer> set4 = (Set2<Integer>) finder4.intersectSets(unions4);
+		System.out.println("Final set by P4: " + set4);	
 		
 		
 		
--- DataStructuresP1/src/solutions/P4Solution.java
+++ DataStructuresP1/src/solutions/P4Solution.java
@@ -1,5 +1,51 @@
 package solutions;
 
-public class P4Solution {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import javax.naming.InvalidNameException;
+
+import interfaces.MySet;
+import mySetImplementations.Set2;
+import setIntersectionFinders.AbstractIntersectionFinder;
+
+public class P4Solution<E> extends AbstractIntersectionFinder<E>  {
+
+	public P4Solution(String name) throws InvalidNameException {
+		super(name);
+		if(!name.equals("4")) throw new InvalidNameException("This is not an implemented set.");
+
+	}
+
+
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public MySet<E> intersectSets(MySet<E>[] t) {
+
+		
+		ArrayList<E> allElements = new ArrayList<>();
+
+
+				for(MySet<E> subset: t){
+					Iterator<E> iter = subset.iterator();
+				while(iter.hasNext())
+				allElements.add(iter.next());
+			
+				}
+			
+		HashMap<Integer, Integer> map = new HashMap<>(); 
+		for (E e : allElements) { 
+			Integer c = map.getOrDefault(e, 0); 
+			map.put((Integer) e, c+1); 
+		}
+		MySet<E> theSet = new Set2<>(); 
+		for (Map.Entry<Integer, Integer> entry : map.entrySet())
+			if (entry.getValue() == t.length) 
+				theSet.add((E)entry.getKey());
+
+		return theSet;
+	}
 }
