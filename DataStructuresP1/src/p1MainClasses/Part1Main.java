84f2ec57349e3843a2d814d37206298b8e2ee9a2
kevin.blakeley <kevin.blakeley@CRAIWS07.ecenet.ece.uprm.edu>
2018-03-22 21:40:51 -0400

    P1 and P1 complete

--- DataStructuresP1/src/p1MainClasses/Part1Main.java
+++ DataStructuresP1/src/p1MainClasses/Part1Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import dataGenerator.DataReader;
 import interfaces.MySet;
 import mySetImplementations.Set1;
+import mySetImplementations.Set2;
 import solutions.P1andP2;
 
 public class Part1Main<E> {
@@ -12,12 +13,19 @@ public class Part1Main<E> {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		DataReader dr = new DataReader();
-		P1andP2<Integer> finder = new P1andP2<Integer>("1");
 		Integer[][][] data = (Integer[][][]) dr.readDataFiles();
 		
-		Set1<Integer>[] unions = (Set1<Integer>[]) toSetArray1(data);
+		P1andP2<Integer> finder = new P1andP2<Integer>("1");
+		P1andP2<Integer> finder2 = new P1andP2<Integer>("2");
+		
+		
+		MySet<Integer>[] unions = (MySet<Integer>[]) toSetArray1(data);
 		Set1<Integer> set = (Set1<Integer>) finder.intersectSets(unions);
 		System.out.println("Final set by P1: " + set);	
+		
+		MySet<Integer>[] unions2 = (MySet<Integer>[]) toSetArray1(data);
+		Set2<Integer> set2 = (Set2<Integer>) finder2.intersectSets(unions2);
+		System.out.println("Final set by P2: " + set2);	
 	}
 	
 	private static <E> Object[] toSetArray1(E[][][] data){
